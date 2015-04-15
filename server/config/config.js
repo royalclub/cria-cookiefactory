a8fd4cefddd34376f763853afd4c420ed40a3360
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-15 20:04:41 +0200

    spring cleaning

--- server/config/config.js
+++ server/config/config.js
@@ -5,19 +5,14 @@ module.exports = {
         port: 3000,                             // change with your port number
         debug: true                             // set debug to true|false
     },
-    test1: {
-        db: 'mongodb://localhost/books-tst1',   // change books with your database
+    test: {
+        db: 'mongodb://localhost/books-tst',    // change books with your database
         port: 3001,                             // change with your port number
-        debug: true                             // set debug to true|false
-    },
-    test2: {
-        db: 'mongodb://localhost/books-tst2',   // change books with your database
-        port: 3002,                             // change with your port number
         debug: true                             // set debug to true|false
     },
     acceptance: {
         db: 'mongodb://localhost/books-acc',    // change books with your database
-        port: 3003,                             // change with your port number
+        port: 3002,                             // change with your port number
         debug: true                             // set debug to true|false
     },
     production: {}
