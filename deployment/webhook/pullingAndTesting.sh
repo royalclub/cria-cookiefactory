9ed0afa85d2f1b6ac3efee853b72e0942b8d03c4
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-15 21:24:16 +0200

    updated

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-echo 123;
+export STAGE0=development
+export STAGE1=test-static-analyzer-passed
+export STAGE2=test-unit-tests-passed
+export STAGE3=acceptance
+export STAGE4=production
+export TESTDIR=../../tests
+export JSLINT=./$TESTDIR/static-analyzer/node_modules/jslint
+
+#########################################
+# STAGE0, development
+#########################################
+git checkout $STAGE0
+git pull
+
+#########################################
+# STAGE1, static-analyzer
+#########################################
+
+# make sure jslint is installed
+if [[ ! -d $JSLINT ]]; then
+	#install jslint locally
+	echo "Please install jslint first."
+	echo "  jslint is expected to be installed in $TESTDIR/static-analyzer/."
+	exit 1
+fi
+
+rm ./$TESTDIR/static-analyzer/error_log.txt
+cd ./$TESTDIR/static-analyzer
+./run_lint.sh
+
+if [ -f ./$TESTDIR/static-analyzer/error_log.txt ]; then
+	echo "=~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~="
+	echo "  Did not pass the static analyse"
+	exit 1
+fi
+
+
+
+git pull $STAGE0
