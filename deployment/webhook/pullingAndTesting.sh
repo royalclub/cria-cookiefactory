677b6f349a60f245fde732823ed49a7ca9a777aa
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-16 22:02:11 +0200

    spring cleaning

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -47,7 +47,7 @@ git merge --no-edit $STAGE0 | tee -a "$DIR/log.log"
 git commit -am "Merging from $STAGE0 to $STAGE1: `date`" | tee -a "$DIR/log.log"
 
 cd $TESTDIR/static-analyzer
-./run_linst.sh > static-analyzer-results.log
+./run_lint.sh > static-analyzer-results.log
 
 if [ -f $TESTDIR/static-analyzer/error_log.txt ]; then
 	echo "=~=~=~=~= ERRORS: No commit for branch 'test' was performed. =~=~=~=~=" | tee -a "$DIR/log.log"
