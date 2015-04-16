1d920f8f8f1f886b23b14aa2da93c210b216dc28
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-16 22:36:22 +0200

    spring cleaning

--- deployment/webhook/server.js
+++ deployment/webhook/server.js
@@ -81,11 +81,6 @@ app.post('/webhook', function (req, res) {
     if (req.body.repository.url === config.repoUrl) {
         console.log('>>>>>req', req.body, '<<<<<<');
         console.log('Now do a git pull for the current branch');
-
-        //// executes `git pull`
-        //child = exec("git pull", cb);
-
-
         child = exec("./pullingAndTesting.sh", cb);
 
     }
