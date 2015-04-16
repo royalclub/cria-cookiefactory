2c5b8e80f262ac08dbeb46baecc605fd73bf4d27
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-16 16:37:48 +0200

    spring cleaning

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -75,9 +75,9 @@ UNIT_TEST_ERRORS=`grep -c 'fail' test-results.log`
 if [ -z $UNIT_TEST_ERRORS ]; then
     echo echo "=~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~=" | tee -a "$DIR/log.log"
 	echo "  Could not execute the tests" | tee -a "$DIR/log.log"
-	echo " " >> "$DIR/log.log"
-	echo " " >> "$DIR/log.log"
-	echo "--------" >> "$DIR/log.log"
+	echo " " | tee -a "$DIR/log.log"
+	echo " " | tee -a "$DIR/log.log"
+	echo "--------"  >> "$DIR/log.log"
 	cat log.log >> "$DIR/log.log"
     exit 1
 fi
