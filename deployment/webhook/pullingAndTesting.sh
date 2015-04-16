441fa1465bbed927597a362c8a0809c3d269356d
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-16 16:53:45 +0200

    Merging from development to test-static-analyzer-passed: Thu Apr 16 16:53:45 CEST 2015

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -66,7 +66,7 @@ git checkout $STAGE2 | tee -a "$DIR/log.log"
 
 echo "Working should be $TESTDIR/unit-tests" | tee -a "$DIR/log.log"
 
-cd $TESTDIR/unit-tests | tee -a "$DIR/log.log"
+cd "$TESTDIR/unit-tests" | tee -a "$DIR/log.log"
 
 echo "Working directory is `pwd`" | tee -a "$DIR/log.log"
 
