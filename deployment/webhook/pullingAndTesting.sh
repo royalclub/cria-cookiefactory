6cb7761035c15f4bd91f9f9666b503496d3f893f
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-17 08:20:59 +0200

    spring cleaning

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -15,9 +15,9 @@ export PARENT_COMMAND=$(ps $PPID | tail -n 1 | awk "{print \$5}")
 echo "`date` Executed by $PARENT_COMMAND" | tee -a "$PWD/log.log"
 
 echo
-echo "`date` #########################################" | tee -a "$DIR/log.log"
-echo "`date` # Preflight checks" | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+echo "`date` - Preflight checks" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
 echo
 echo "`date` make sure jslint is installed" | tee -a "$DIR/log.log"
 if [[ ! -d $JSLINT ]]; then
@@ -28,22 +28,21 @@ if [[ ! -d $JSLINT ]]; then
 fi
 
 echo | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
-echo "`date` # STAGE0, development" | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+echo "`date` - STAGE0, development" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
 echo | tee -a "$DIR/log.log"
 
 git checkout $STAGE0 | tee -a "$DIR/log.log"
 git pull | tee -a "$DIR/log.log"
 
 echo | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
-echo "`date` # STAGE1, static-analyzer" | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+echo "`date` - STAGE1, static-analyzer" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
 echo | tee -a "$DIR/log.log"
 
 git checkout $STAGE1 | tee -a "$DIR/log.log"
-
 git merge --no-edit $STAGE0 | tee -a "$DIR/log.log"
 git commit -am "Merging from $STAGE0 to $STAGE1: `date`" | tee -a "$DIR/log.log"
 
@@ -51,8 +50,8 @@ cd $TESTDIR/static-analyzer
 ./run_lint.sh > static-analyzer-results.log
 
 if [ -f $TESTDIR/static-analyzer/error_log.txt ]; then
-	echo "`date` =~=~=~=~= ERRORS: No commit for branch 'test' was performed. =~=~=~=~=" | tee -a "$DIR/log.log"
-	echo "`date` =~=~=~=~= Resolve the conflicts before continuing.           =~=~=~=~=" | tee -a "$DIR/log.log"
+	echo "`date` !!!!! ERRORS: No commit for branch 'test' was performed. !!!!!" | tee -a "$DIR/log.log"
+	echo "`date` >>>>> Resolve the conflicts before continuing." | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
 	exit 1
 fi
@@ -62,9 +61,11 @@ git commit -am "TEST: `date`" | tee -a "$DIR/log.log"
 
 git push origin $STAGE1 | tee -a "$DIR/log.log"
 
-echo "`date` #########################################" | tee -a "$DIR/log.log"
-echo "`date` # STAGE2, unit-tests" | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+echo "`date` - STAGE2, unit-tests" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+
+git checkout $STAGE2 | tee -a "$DIR/log.log"
 
 cd "$TESTDIR/../server"
 export NODE_ENV=test
@@ -73,7 +74,6 @@ export node_PID=$!
 sleep 4
 echo "`date` node started with process id = $node_PID" | tee -a log.log
 
-git checkout $STAGE2 | tee -a "$DIR/log.log"
 
 # Change directory to unit-tests
 cd "$TESTDIR/unit-tests"
@@ -90,22 +90,22 @@ kill -9 $node_PID
 export TEST_FAILURUES=`grep -ci 'fail' unit-tests-results.log`
 
 if [ -z "$TEST_FAILURUES" ]; then
-    echo "`date` =~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~=" | tee -a "$DIR/log.log"
-	echo "`date`   Could not execute the tests. Variable TEST_FAILURUES=$TEST_FAILURUES" | tee -a "$DIR/log.log"
+    echo "`date` !!!!! ERRORS ERRORS ERRORS !!!!!" | tee -a "$DIR/log.log"
+	echo "`date` >>>>>  Could not execute the tests. Variable TEST_FAILURUES=$TEST_FAILURUES" | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
     exit 1
 fi
 
 if [ $TEST_FAILURUES -ne 0 ]; then
-    echo"`date` =~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~=" | tee -a "$DIR/log.log"
+    echo"`date` !!!!! ERRORS ERRORS ERRORS !!!!!" | tee -a "$DIR/log.log"
 	echo "`date`   Did not pass the unit-tests" | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
 	exit 1
 fi
 
 if [ -f ./test/static-analyzer/error_log.txt ]; then
-	echo "=~=~=~=~= ERRORS: No commit for branch 'test' was performed. =~=~=~=~=" | tee -a "$DIR/log.log"
-	echo "=~=~=~=~= Resolve the conflicts before continuing.           =~=~=~=~=" | tee -a "$DIR/log.log"
+	echo "!!!!! ERRORS: No commit for branch 'test' was performed. !!!!!" | tee -a "$DIR/log.log"
+	echo "!!!!! >>>>> Resolve the conflicts before continuing.           !!!!!" | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
 	exit 1
 fi
@@ -116,13 +116,12 @@ git commit -am "Merging from $STAGE2 to $STAGE2: `date`" | tee -a "$DIR/log.log"
 git push origin $STAGE2 | tee -a "$DIR/log.log"
 
 echo | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
-echo "`date` # STAGE3, end to end" | tee -a "$DIR/log.log"
-echo "`date` #########################################" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
+echo "`date` - STAGE3, end to end" | tee -a "$DIR/log.log"
+echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/log.log"
 echo | tee -a "$DIR/log.log"
 
 git checkout $STAGE3 | tee -a "$DIR/log.log"
-
 git merge --no-edit $STAGE2 | tee -a "$DIR/log.log"
 git commit -am "Merging from $STAGE2 to $STAGE3: `date`" | tee -a "$DIR/log.log"
 
@@ -153,14 +152,14 @@ kill -9 $selenium_PID
 export TEST_FAILURUES=`grep -ci 'fail' end-to-end-results.log`
 
 if [ -z "$TEST_FAILURUES" ]; then
-    echo "`date` =~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~=" | tee -a "$DIR/log.log"
+    echo "`date` !!!!! ERRORS ERRORS ERRORS !!!!!" | tee -a "$DIR/log.log"
 	echo "`date`   Could not execute the tests. Variable TEST_FAILURUES=$TEST_FAILURUES" | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
     exit 1
 fi
 
 if [ $TEST_FAILURUES -ne 0 ]; then
-    echo"`date` =~=~=~=~= ERRORS ERRORS ERRORS =~=~=~=~=" | tee -a "$DIR/log.log"
+    echo"`date` !!!!! ERRORS ERRORS ERRORS !!!!!" | tee -a "$DIR/log.log"
 	echo "`date`   Did not pass the end-to-end tests." | tee -a "$DIR/log.log"
 	git checkout $STAGE0 | tee -a "$DIR/log.log"
 	exit 1
