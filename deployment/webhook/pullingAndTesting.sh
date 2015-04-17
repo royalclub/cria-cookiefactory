5d6c00753dd9b47144eb23412eb42619cb1bf0a9
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-17 09:06:24 +0200

    spring cleaning

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -164,7 +164,6 @@ git merge --no-edit $STAGE2 | tee -a "$DIR/$CUR_SCRIPT"
 git commit -am "Merging from $STAGE2 to $STAGE3: `date`" | tee -a "$DIR/$CUR_SCRIPT"
 git push origin $STAGE3 | tee -a "$DIR/$CUR_SCRIPT"
 
-
 echo | tee -a "$DIR/$CUR_SCRIPT"
 echo "`date` -------------------------------------------------------------------------------" | tee -a "$DIR/$CUR_SCRIPT"
 echo "`date` - All stages successfull tested." | tee -a "$DIR/$CUR_SCRIPT"
