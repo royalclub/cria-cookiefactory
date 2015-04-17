4fbf0c9cce830599baf3615613b16d2de430deff
Theo Theunissen <theo.theunissen@gmail.com>
2015-04-17 13:34:57 +0200

    improved tests in spring cleaning

--- deployment/webhook/pullingAndTesting.sh
+++ deployment/webhook/pullingAndTesting.sh
@@ -135,7 +135,7 @@ echo "`date` node started with process id = $node_PID" | tee -a $CUR_SCRIPT
 
 # start up selenium-stand-alone
 selenium-standalone start --version=2.43.1 >/dev/null 2>&1 &
-sleep 4
+sleep 60
 export selenium_PID=$!
 
 # run e2e tests
