455bc34aaf6d51d42c8c40eb0d4e6c3e9e824f53
Joeri Harleman <me@joeriharleman.nl>
2015-05-13 14:29:29 +0200

    Updated cookie model format

--- server/app/models/cookies.js
+++ server/app/models/cookies.js
@@ -10,11 +10,11 @@
         schemaName;
 
     schemaName = new Schema({
-        cookieName: {type: String, required: true},
-        cookieLayers: {type: String},
-        modificationDate: {type: Date, "default": Date.now}
-    }, {collection: "cookies"});
-    
+        cookieName: { type: String, required: true },
+        cookieLayers: { type: String },
+        modificationDate: { type: Date, "default": Date.now }
+    }, { collection: "cookies" });
+
     module.exports = mongoose.model('Cookie', schemaName);
 
 }());
