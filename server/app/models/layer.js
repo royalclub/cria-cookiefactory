2c0b64bc16b14899d5ac97c5fcc9248fceb61555
Bart van Heesch <bartje999@gmail.com>
2015-05-18 13:15:29 +0200

    layerModel

--- server/app/models/layer.js
+++ server/app/models/layer.js
@@ -8,7 +8,6 @@
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
         ingredientSchema = mongoose.model('Ingredient'),
-        shapeSchema = mongoose.model('Shape'),
         schemaName;
 
     schemaName = new Schema({
