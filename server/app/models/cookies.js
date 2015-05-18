7ffc560fb3e96dd33d3c1c449d9daed5b6031986
¨Tom <Konink1992@gmail.com>
2015-05-18 13:37:18 +0200

    update file

--- server/app/models/cookies.js
+++ server/app/models/cookies.js
@@ -13,11 +13,10 @@
 
     schemaName = new Schema({
         cookieName: {type: String, required: true},
-<<<<<<< HEAD
         cookieShape: {type: shapeSchema.schema},
         cookieLayers: {type: String},
-        cookieCrearor: {type: String, required: true},
-        creationDate {type: Date, "default": Date.now},
+        cookieCreator: {type: String, required: true},
+        creationDate: {type: Date, "default": Date.now},
         modificationDate: {type: Date, "default": Date.now}
     }, {collection: "cookies"});
 
@@ -25,12 +24,6 @@
     modelName = 'Cookie';
     mongoose.model(modelName, schemaName);
 
-}());
-=======
-        cookieLayers: {type: String},
-        modificationDate: {type: Date, "default": Date.now}
-    },
-            {collection: "cookies"});
+
     module.exports = mongoose.model('Cookie', schemaName);
 }());
->>>>>>> d0ac018d9512e479b036676ce33f8151a038ceb4
