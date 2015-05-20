1edce220ee9101483eb68bc9324f483ed6f6f3b0
Joeri Harleman <me@joeriharleman.nl>
2015-05-20 15:08:25 +0200

    Fixed linter issues

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -9,14 +9,14 @@
 
     userSchema = new mongoose.Schema({
         username: {type: String, required: true, minlength: 4, maxlength: 30},
-		salt: {type: String, required: true, minlength: 12, maxlength: 12},
-		password: {type: String, required: true},
+        salt: {type: String, required: true, minlength: 12, maxlength: 12},
+        password: {type: String, required: true},
         firstname: {type: String, required: true, minlength: 2, maxlength: 50},
         tussenvoegsels: {type: String, maxlength: 10},
         lastname: {type: String, required: true, minlength: 2, maxlength: 50},
         dateOfBirth: {type: Date},
-		emailAddress: {type: String, required: true},
-		addresses: [{type: addressModel, required: true}],
+        emailAddress: {type: String, required: true},
+        addresses: [{type: addressModel, required: true}],
         creationDate: {type: Date, "default": Date.now, required: true},
         modificationDate: {type: Date, "default": Date.now, required: true}
     }, {collection: "users"});
