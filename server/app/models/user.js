9b9291e01a973cb6c4b3e29fbfc33cc8d0b3d174
Joeri Harleman <me@joeriharleman.nl>
2015-05-21 21:04:35 +0200

    Fixed Regular Expression issues (JSLint)

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -15,7 +15,7 @@
         inserts: {type: String, maxlength: 10},
         lastName: {type: String, required: true, minlength: 2, maxlength: 50},
         dateOfBirth: {type: Date},
-        emailAddress: {type: String, required: true, match: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i, unique: true},
+        emailAddress: {type: String, required: true, match: /^[\-a-z0-9~!$%\^&*_=+}{\'?]+(\.[\-a-z0-9~!$%\^&*_=+}{\'?]+)*@([a-z0-9_][\-a-z0-9_]*(\.[\-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i, unique: true},
         addresses: [address.schema],
         creationDate: {type: Date, "default": Date.now, required: true},
         modificationDate: {type: Date, "default": Date.now, required: true}
