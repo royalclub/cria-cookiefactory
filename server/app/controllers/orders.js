eba280c72b8918cd81cb2906b987c834369ea52b
Joeri Harleman <me@joeriharleman.nl>
2015-05-21 15:36:29 +0200

    Several changes.

--- server/app/controllers/orders.js
+++ server/app/controllers/orders.js
@@ -3,42 +3,18 @@
 "use strict";
 
 var mongoose = require('mongoose'),
-    Order = mongoose.model('Order');
+    Order = mongoose.model('Order'),
+    OrderRule = mongoose.model('OrderRule'),
+    OrderStatus = mongoose.model('OrderStatus'),
+    OrderUser = mongoose.model('OrderUser'),
+    Address = mongoose.model('Address');
 
 /**
- * CREATE a book
- * --------------------
- * Controller to create a book.
- *
- * Instructions, hints and questions
- * - Read about the 'save' method from Mongoose.
- * - Use the 'save' method from Mongoose.
- *   - Question: What are the differences between MongoDb and Mongoose?
- *   - Question: explain the concepts of 'schema type' and 'model'. How are they related?
- * - Return all fields.
- * - Use the model "Book".
- *
- * The return object has three properties:
- *
- * - meta: These are all optional and free to extend
- *   - method name: The name of the method
- *   - timestamp
- *   - filename: The name of the file. Use '__filename' for this.
- *   - duration: Duration of execution, time spend on server or other meaningful metric
- * - doc: The result object, in case of retrieving all objects, this is always an array. No documents is returned as an empty array.
- * - err: If no errors, it has the value 'null'
- *
- * Errors are not thrown in the node application but returned to the user.
- * - Question: What will happen if you throw an error on the server?
- * @param req
- * @param res
- * @see http://docs.mongodb.org/manual/reference/method/db.collection.save/
- * @see http://mongoosejs.com/docs/api.html#model_Model-save
- * @module books/create
+ * Create a new Layer.
+ * @param req   The request information.
+ * @param res   The result object.
  */
-
 exports.create = function (req, res) {
-
     var doc = new Order(req.body);
 
     doc.save(function (err) {
@@ -54,51 +30,21 @@ exports.create = function (req, res) {
         };
 
         return res.send(retObj);
-
     });
-
 };
 
+
 /**
- * RETRIEVE _all_ books
- * --------------------
- * Controller to retrieve _all_ books.
- *
- * Instructions, hints and questions
- * - Read about the 'find' method from Mongoose.
- * - Use the 'save' method from Mongoose.
- *   - Question: What are the differences between MongoDb and Mongoose?
- * - The 'query' parameter is an empty object.
- *   - Question: Why is it empty?
- * - Skip the options.
- *   - Question: Describe the options.
- * - Return all fields.
- * - Use the model "Book".
- *
- * The return object has three properties:
- *
- * - meta: These are all optional and free to extend
- *   - method name: The name of the method
- *   - timestamp
- *   - filename: The name of the file. Use '__filename' for this.
- *   - duration: Duration of execution, time spend on server or other meaningful metric
- * - doc: The result object, in case of retrieving all objects, this is always an array. No documents is returned as an empty array.
- * - err: If no errors, it has the value 'null'
- *
- * Errors are not thrown in the node application but returned to the user.
- * - Question: What will happen if you throw an error on the server?
- * @param req
- * @param res
- * @see http://docs.mongodb.org/manual/reference/method/db.collection.find/
- * @see http://mongoosejs.com/docs/api.html#model_Model.find
- * @module books/list
+ * Retrieve a list of _all_ Orders.
+ * @param req   The request information.
+ * @param res   The result object.
  */
 exports.list = function (req, res) {
     var conditions, fields, sort;
 
     conditions = {};
     fields = {};
-    sort = {'orderNumber': -1};
+    sort = {'creationDate': 1};
 
     Order
         .find(conditions, fields)
@@ -116,42 +62,13 @@ exports.list = function (req, res) {
             };
 
             return res.send(retObj);
-
         });
 };
 
 /**
- * RETRIEVE _one_ book
- * --------------------
- * Controller to retrieve _one_ books.
- *
- * Instructions, hints and questions
- * - Read about the 'findOne' method from Mongoose.
- * - Use the 'findOne' method from Mongoose.
- *   - Question: What is de result object from findOne?
- *   - Question: What are the differences between MongoDb and Mongoose?
- * - The 'query' parameter is an empty object.
- *   - Question: Why is it empty?
- * - Skip the options.
- * - Return all fields.
- * - Use the model "Book".
- * Question: Define route parameters and body parameter. What are the differences?
- *
- * The return object has three properties:
- *
- * - meta: These are all optional and free to extend
- *   - method name: The name of the method
- *   - timestamp
- *   - filename: The name of the file. Use '__filename' for this.
- *   - duration: Duration of execution, time spend on server or other meaningful metric
- * - doc: The result object is either an object or null.
- * - err: If no errors, it has the value 'null'
- *
- * @module books/detail
- * @param req
- * @param res
- * @see http://docs.mongodb.org/manual/reference/method/db.collection.findOne/
- * @see http://mongoosejs.com/docs/api.html#model_Model.findOne
+ * Retrieve details of a _single_ Order.
+ * @param req   The request information.
+ * @param res   The result object.
  */
 exports.detail = function (req, res) {
     var conditions, fields;
@@ -164,7 +81,7 @@ exports.detail = function (req, res) {
         .exec(function (err, doc) {
             var retObj = {
                 meta: {"action": "detail", 'timestamp': new Date(), filename: __filename},
-                doc: doc, // only the first document, not an array when using "findOne"
+                doc: doc,
                 err: err
             };
             return res.send(retObj);
@@ -172,27 +89,27 @@ exports.detail = function (req, res) {
 };
 
 /**
- * Fetch _one_ record based on an id.
- * @param req
- * @param res
+ * Update a single Order.
+ * @param req   The request information.
+ * @param res   The result object.
  */
 exports.updateOne = function (req, res) {
 
     var conditions =
         {_id: req.params._id},
         update = {
-            orderNumber: req.body.orderNumber || '',
-            orderStatus: req.body.orderStatus || '',
-            orderUsername: req.body.orderUsername || '',
-            orderRules: req.body.orderRules || '',
-            orderInvoiceAddress: req.body.orderInvoiceAddress || '',
-            orderShipmentAddress: req.body.orderShipmentAddress || '',
-            orderVat: req.body.orderVat || '',
-            orderTotal: req.body.orderTotal || '',
-            orderModificationDate: new Date()
+            "number": req.body.number,
+            status: req.body.status,
+            user: req.body.user,
+            rules: req.body.rules,
+            invoiceAddress: req.body.invoiceAddress,
+            shipmentAddress: req.body.shipmentAddress,
+            vatPercentage: req.body.vatPercentage,
+            modificationDate: new Date()
         },
         options = {multi: false},
         callback = function (err, doc) {
+
             var retObj = {
                 meta: {
                     "action": "update",
@@ -213,44 +130,9 @@ exports.updateOne = function (req, res) {
 };
 
 /**
- * DELETE
- * remove @ http://mongoosejs.com/docs/api.html#model_Model-remove
- * @param req
- * @param res
- */
-
-/**
- * DELETE _one_ book
- * --------------------
- * Controller to delete _one_ books.
- *
- * Instructions, hints and questions
- * - Read about the 'findOne' method from Mongoose.
- * - Use the 'findOne' method from Mongoose.
- *   - Question: What is de result object from findOne?
- *   - Question: What are the differences between MongoDb and Mongoose?
- * - The 'query' parameter is an empty object.
- *   - Question: Why is it empty?
- * - Skip the options.
- * - Return all fields.
- * - Use the model "Book".
- * Question: Define route parameters and body parameter. What are the differences?
- *
- * The return object has three properties:
- *
- * - meta: These are all optional and free to extend
- *   - method name: The name of the method
- *   - timestamp
- *   - filename: The name of the file. Use '__filename' for this.
- *   - duration: Duration of execution, time spend on server or other meaningful metric
- * - doc: The result object is either an object or null.
- * - err: If no errors, it has the value 'null'
- *
- * @module books/detail
- * @param req
- * @param res
- * @see http://docs.mongodb.org/manual/reference/method/db.collection.remove/
- * @see http://mongoosejs.com/docs/api.html#model_Model.remove
+ * Delete a single Order.
+ * @param req   The request information.
+ * @param res   The result object.
  */
 exports.deleteOne = function (req, res) {
     var conditions, callback, retObj;
