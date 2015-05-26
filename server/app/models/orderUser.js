43f2b6bb17f43d3ac9a12cce73264778bca43a77
Mitch <mvanhuet@live.nl>
2015-05-26 14:57:38 +0200

    Updated Unit tests added unit test orders

--- server/app/models/orderUser.js
+++ server/app/models/orderUser.js
@@ -7,7 +7,7 @@
         orderUserSchema;
 
     orderUserSchema = new mongoose.Schema({
-        username: {type: String, required: true, minlength: 4, maxlength: 3},
+        username: {type: String, required: true, minlength: 4, maxlength: 30},
         emailAddress: {type: String, required: true, match: /^[\-a-z0-9~!$%\^&*_=+}{\'?]+(\.[\-a-z0-9~!$%\^&*_=+}{\'?]+)*@([a-z0-9_][\-a-z0-9_]*(\.[\-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i},
         firstName: {type: String, required: true, minlength: 2, maxlength: 50},
         inserts: {type: String},
