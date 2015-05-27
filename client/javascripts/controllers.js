c59d6dd3919cfb7b4ba7d42c485a29c3dd8caf4b
Joeri Harleman <me@joeriharleman.nl>
2015-05-27 14:51:37 +0200

    Responsive

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -73,11 +73,14 @@ cookieFactory.controller('myCtrl', function ($scope) {
 
 cookieFactory.controller('menuController', function ($scope) {
     "use strict";
-
     $scope.items = [
         {name: "Home", url: "/#/", active: "active"},
         {name: "Over ons", url: "/#/about", active: ""},
         {name: "Ontwerp koekje", url: "/#/cookies/design", active: ""},
         {name: "Contact", url: "/#/contact", active: ""}
+    ];
+    $scope.breadcrumb = [
+        {name: "Cookie Factory", url: "/#/", active: "active"},
+        {name: "Over ons", url: "/#/about", active: "active"}
     ];
 });
