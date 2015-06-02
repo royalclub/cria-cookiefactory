da3145501e943e9b81f2681ac7da36ec3dc681bd
Joeri Harleman <me@joeriharleman.nl>
2015-06-02 10:05:44 +0200

    Linter fix

--- server/authentication.js
+++ server/authentication.js
@@ -1,3 +1,5 @@
+/*jslint node: true */
+
 (function () {
     "use strict";
 
@@ -6,14 +8,14 @@
         LocalStrategy = require('passport-local').Strategy,
         User = require('./app/models/users.js').model;
 
-    passport.serializeUser(function(user, done) {
-      done(null, user.id);
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
     });
 
-    passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
-        done(err, user);
-      });
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
     });
 
     module.exports = new LocalStrategy(
@@ -21,15 +23,14 @@
             var saltConditions = {
                 'username': username
             };
-
             User.findOne(saltConditions, function (err, user) {
-                if (err) { 
+                if (err) {
                     return done(err);
                 }
                 if (!user) {
                     return done(null, false, { message: 'Gebruikersnaam niet gevonden.' });
                 }
-                if (!user.validPassword(password)) { 
+                if (!user.validPassword(password)) {
                     return done(null, false, { message: 'Wachtwoord ongeldig.' });
                 }
                 return done(null, user);
