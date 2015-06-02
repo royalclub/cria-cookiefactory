eeb826a4e91908d36708b9f3f58dc160f24769d9
Bartje999 <bartje999@gmail.com>
2015-06-02 11:35:26 +0200

    menuTest

--- client/javascripts/controllers/menuController.js
+++ client/javascripts/controllers/menuController.js
@@ -8,22 +8,25 @@
  */
 //var cookieFactory = angular.module('cookieFactory', []);
 var activePage = 0;
-    
+
 cookieFactory.controller('menuController', function ($scope, $routeParams) {
     "use strict";
     $scope.menuItems = [
         {name: "Home", url: "/#/", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: "active"}]},
-        {name: "Over ons", url: "/#/about", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""},{name: "Over ons", url: "/#/about", active: "active"}]},
-        {name: "Ontwerp koekje", url: "/#/cookies/design", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""},{name: "Ontwerp Koekje", url: "/#/cookies/design", active: "active"}]},
-        {name: "Contact", url: "/#/contact", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""},{name: "Contact", url: "/#/contact", active: "active"}]}
+        {name: "Over ons", url: "/#/about", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""}, {name: "Over ons", url: "/#/about", active: "active"}]},
+        {name: "Ontwerp koekje", url: "/#/cookies/design", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""}, {name: "Ontwerp Koekje", url: "/#/cookies/design", active: "active"}]},
+        {name: "Contact", url: "/#/contact", active: "", breadcrumb: [{name: "Cookie Factory", url: "/#/", active: ""}, {name: "Contact", url: "/#/contact", active: "active"}]}
     ];
-    $scope.SetActivePage = function(index){
+    $scope.SetActivePage = function (index) {
         activePage = index;
     };
-    $scope.GetBreadcrumb = function(){
+    $scope.GetBreadcrumb = function () {
         return $scope.menuItems[activePage].breadcrumb;
     };
-    $scope.IsActive = function(name) {
-        if (name == $scope.menuItems[activePage].name) return "active"; else return "";
+    $scope.IsActive = function (name) {
+        if (name === $scope.menuItems[activePage].name) {
+            return "active";
+        }
+        return "";
     };
 });
