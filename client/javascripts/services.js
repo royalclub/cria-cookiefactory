9f29368012886cad0e952392c4ef3bcc3597d85a
Mitch <mvanhuet@live.nl>
2015-06-04 15:54:55 +0200

    Added new design

--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -69,5 +69,15 @@
                         });
                     }
                 };
-            }]);
+            }])
+        .factory('cookies', function(){
+                  var cookies = {};
+                
+                  cookies.list = [];
+                
+                  cookies.add = function(cookie){
+                    cookies.list.push({id: cookies.list.length, text: cookie});
+                  };
+                  return cookies;
+            });
 }());
