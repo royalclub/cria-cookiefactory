b87dc6ccebf02c6700493792d3ea66d44428d1de
Bartje999 <bartje999@gmail.com>
2015-06-10 11:20:20 +0200

    Cart fix contact page

--- client/javascripts/controllers/cartController.js
+++ client/javascripts/controllers/cartController.js
@@ -19,7 +19,7 @@ cookieFactory.controller('cartController', function ($scope, $location) {
     function calculatePrices() {
         var subtotal = 0.0, i;
         for (i = 0; i < $scope.cartItems.length; i += 1) {
-            subtotal += $scope.orderRules[i].cookie.price * $scope.orderRules[i].amountOfBoxes;
+            subtotal += $scope.orderRules[i].cookie[0].price * $scope.orderRules[i].amountOfBoxes;
         }
         $scope.subtotal = subtotal + $scope.shipping;
         $scope.tax = (subtotal / 100) * 21;
@@ -43,7 +43,7 @@ cookieFactory.controller('cartController', function ($scope, $location) {
             for (b = 0; b < $scope.cartItems.length; b += 1) {
                 $scope.cartItems[b].price = 0;
                 for (layer = 0; layer < $scope.cartItems[b].layers.length; layer += 1) {
-                    $scope.cartItems[b].price = $scope.cartItems[b].layers[layer].options.price + $scope.cartItems[b].price;
+                    $scope.cartItems[b].price = $scope.cartItems[b].layers[layer].options[0].price + $scope.cartItems[b].price;
                 }
                 $scope.cartItems[b].amount = 1;
             }
